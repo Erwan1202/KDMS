2a27d6849c701080383108baf49043e1080ca280
Erwan1202 <erwan1202@users.noreply.github.com>
2026-04-08 08:08:30 +0200

    fix: use output server for SSR page support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: node({ mode: 'standalone' }),
   site: 'https://www.k-dms.co',
   integrations: [sitemap()],
